23f7341f13f322b0e3f1f4ee9655764c474e615e
root <root@4wing49.cpiv.com>
2018-09-28 16:35:15 +0530

    sample python function

--- basic1.py
+++ basic1.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
-print("Hello")
+
+def get_even():
+    '''
+    Function to print even numbers
+    '''
+    even = []
+    for i in range(10):
+        if i%2 == 0:
+            even.append(i)
+    return even
+
+print get_even()
